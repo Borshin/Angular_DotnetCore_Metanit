86ba28120059c7d677e1bbb6d3a0e219a92a925d
alexxx-gt <alexxx-gt@yandex.ru>
2019-01-27 19:55:37 +0200

    Add project files.

---
+++ Angular_DotNetCore/Models/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class ApplicationContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+	public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+	{
+		
+	}
+}
